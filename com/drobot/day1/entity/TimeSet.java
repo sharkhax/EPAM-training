9af427047716a9cde87b6bf29093336952791fb1
Vladislav <58185373+sharkhax@users.noreply.github.com>
2020-06-11 09:49:10 +0300

    Update TimeSet.java

--- com/drobot/day1/entity/TimeSet.java
+++ com/drobot/day1/entity/TimeSet.java
@@ -36,8 +36,13 @@ public class TimeSet {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         TimeSet timeSet = (TimeSet) o;
 
